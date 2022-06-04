ac2d7c0b9398aca96ae6214ae91896128de9b400
devon.stern <devon.stern@gmail.com>
2022-06-03 20:19:32 -0600

    Basic data persistence with Capacitor Storage
    Not intended for large amounts of data, will need to be replaced
    later.

--- src/items/ItemModel.ts
+++ src/items/ItemModel.ts
@@ -1,4 +1,5 @@
-import { atom } from "recoil"
+import { atom, AtomEffect } from "recoil"
+import { Storage } from "@capacitor/storage";
 
 export interface Item {
 	id: string
@@ -11,7 +12,22 @@ const startingItems: Item[] = [
 	{ id: '3', name: 'Test 3' },
 ]
 
+const itemPersistenceEffect: AtomEffect<Item[]> = ({setSelf, onSet}) => {
+	Storage.get({ key: 'items' })
+		.then(({ value }) => {
+			if (!value) return
+			setSelf(JSON.parse(value))
+		})
+	
+	onSet(newValue => {
+		Storage.set({ key: 'items', value: JSON.stringify(newValue) })
+	})
+}
+
 export const itemsState = atom<Item[]>({
 	key: 'itemsState',
 	default: startingItems,
+	effects: [
+		itemPersistenceEffect,
+	],
 })
