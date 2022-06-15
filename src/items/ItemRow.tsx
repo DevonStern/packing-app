b7521ad4bf2678b80fcbbe31ec37db3a895ed115
devon.stern <devon.stern@gmail.com>
2022-06-15 08:37:44 -0600

    Show assigned persons and item-person state in ItemRow

--- src/items/ItemRow.tsx
+++ src/items/ItemRow.tsx
@@ -1,9 +1,9 @@
-import { IonIcon, IonItem, IonItemOption, IonItemOptions, IonItemSliding } from "@ionic/react"
+import { IonBadge, IonIcon, IonItem, IonItemOption, IonItemOptions, IonItemSliding } from "@ionic/react"
 import { trash } from "ionicons/icons"
 import { useHistory } from "react-router"
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil"
 import { currentListIdState } from "../lists/ListModel"
-import { currentItemIdState, Item, itemsState } from "./ItemModel"
+import { currentItemIdState, Item, itemsState, ItemState } from "./ItemModel"
 
 interface ItemRowProps {
 	item: Item
@@ -31,6 +31,16 @@ const ItemRow: React.FC<ItemRowProps> = ({ item }) => {
 			<IonItemSliding>
 				<IonItem onClick={goToItem}>
 					{item.name}
+					{item.persons.map(ip => (
+						<IonBadge
+							key={ip.person.id}
+							slot="end"
+							mode="ios"
+							color={ip.state === ItemState.LOADED ? 'success' : 'primary'}
+						>
+							{ip.person.name.slice(0, 1)}
+						</IonBadge>
+					))}
 				</IonItem>
 				<IonItemOptions side="start">
 					<IonItemOption color="danger" onClick={deleteItem}>
