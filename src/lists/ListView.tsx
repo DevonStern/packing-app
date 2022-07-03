6d3ef6c738143c220c2ebc61e09a34a0830e8d6d
devon.stern <devon.stern@gmail.com>
2022-07-02 20:22:25 -0600

    Filter items by tag

--- src/lists/ListView.tsx
+++ src/lists/ListView.tsx
@@ -12,6 +12,7 @@ import { multiSelectState, selectedItemsState } from "../state/state"
 import MultiSelectActions from "../general/MultiSelectActions"
 import { getCurrentItemState, getItemStateValues } from "../utils/itemStateUtils"
 import { Item, ItemState } from "../items/itemModels"
+import { Tag, tagsState } from "../tags/tagModel"
 
 interface ListViewProps {
 	list: List
@@ -19,23 +20,36 @@ interface ListViewProps {
 
 interface Filters {
 	itemState?: ItemState[]
+	tag?: Tag[]
 }
 
 const ListView: React.FC<ListViewProps> = ({ list }) => {
 	const isMultiSelectMode = useRecoilValue(multiSelectState)
 	const [selectedItems, setSelectedItems] = useRecoilState(selectedItemsState)
+	const tags = useRecoilValue(tagsState)
 
 	const [isAddItemInputOpen, setIsAddItemInputOpen] = useState<boolean>(false)
 	const [filters, setFilters] = useState<Filters>({})
 
+	useEffect(() => {
+		console.log(filters)
+	}, [filters])
+
 	const filteredItems: Item[] = list.items.filter(item => {
-		if (!filters.itemState || filters.itemState.length === 0) {
-			return true
-		}
-		return filters.itemState.includes(getCurrentItemState(item))
+		const hasItemStateFilter: boolean = filters.itemState !== undefined && filters.itemState.length > 0
+		const hasTagFilter: boolean = filters.tag !== undefined && filters.tag.length > 0
+		console.log('filter', filters.tag)
+		return (
+			(!hasItemStateFilter || filters.itemState!.includes(getCurrentItemState(item)))
+			&& (!hasTagFilter || filters.tag!.some(ft => {
+				return item.tags.some(it => {
+					return it.id === ft.id
+				})
+			}))
+		)
 	})
 
-	const handleItemStateChange = (event: CustomEvent<SelectChangeEventDetail<ItemState[]>>) => {
+	const handleItemStateFilterChange = (event: CustomEvent<SelectChangeEventDetail<ItemState[]>>) => {
 		const updatedItemState: ItemState[] = event.detail.value
 		const updatedFilters: Filters = {
 			...filters,
@@ -44,10 +58,28 @@ const ListView: React.FC<ListViewProps> = ({ list }) => {
 		setFilters(updatedFilters)
 	}
 
+	const handleTagFilterChange = (event: CustomEvent<SelectChangeEventDetail<string[]>>) => {
+		const updatedIds: string[] = event.detail.value
+		const updatedTags: Tag[] = updatedIds.map(id => {
+			const tag: Tag | undefined = tags.find(t => t.id === id)
+			if (!tag) {
+				throw new Error(`Could not find tag with matching ID to the filter - ID: ${id}`)
+			}
+			return tag
+		})
+		const updatedFilters: Filters = {
+			...filters,
+			tag: updatedTags,
+		}
+		setFilters(updatedFilters)
+	}
+
 	return (
 		<>
 			<FilterAccordion
-				onItemStateChange={handleItemStateChange}
+				// filters={filters}
+				onItemStateChange={handleItemStateFilterChange}
+				onTagChange={handleTagFilterChange}
 			/>
 			<IonList>
 				{filteredItems.map(item => {
@@ -83,10 +115,28 @@ export default ListView
 
 
 interface FilterAccordionProps {
+	// filters: Filters
 	onItemStateChange: (event: CustomEvent<SelectChangeEventDetail<ItemState[]>>) => void
+	onTagChange: (event: CustomEvent<SelectChangeEventDetail<string[]>>) => void
 }
 
-const FilterAccordion: React.FC<FilterAccordionProps> = ({ onItemStateChange }) => {
+const FilterAccordion: React.FC<FilterAccordionProps> = ({
+	// filters,
+	onItemStateChange,
+	onTagChange,
+}) => {
+	const tags = useRecoilValue(tagsState)
+
+	// const handleChange = (event: CustomEvent<SelectChangeEventDetail<string[]>>) => {
+	// 	if (typeof event.detail.value === 'string') return
+
+	// 	const updatedIds: string[] = event.detail.value
+	// 	const previousIds: string[] = filters.tag?.map(t => t.id) ?? []
+	// 	if (JSON.stringify(previousIds) === JSON.stringify(updatedIds)) return
+
+	// 	onTagChange(event)
+	// }
+
 	return (
 		<IonAccordionGroup>
 			<IonAccordion>
@@ -100,6 +150,20 @@ const FilterAccordion: React.FC<FilterAccordionProps> = ({ onItemStateChange }) 
 							{getItemStateValues().map((itemState, index) => (
 								<IonSelectOption key={index} value={index}>
 									{itemState}
+								</IonSelectOption>
+							))}
+						</IonSelect>
+					</IonItem>
+					<IonItem>
+						<IonLabel>Tag:</IonLabel>
+						<IonSelect
+							multiple
+							onIonChange={onTagChange}
+							// onIonChange={handleChange}
+						>
+							{tags.map(tag => (
+								<IonSelectOption key={tag.id} value={tag.id}>
+									{tag.name}
 								</IonSelectOption>
 							))}
 						</IonSelect>
