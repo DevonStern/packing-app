bd4e5891bb468c0e9d2acb614c058f608d9c52b1
devon.stern <devon.stern@gmail.com>
2023-07-14 07:54:18 -0600

    Remove ability to delete tags and persons

--- src/persons/PersonsView.tsx
+++ src/persons/PersonsView.tsx
@@ -1,44 +1,22 @@
-import { IonIcon, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonList } from "@ionic/react"
+import { IonItem, IonList } from "@ionic/react"
 import { useState } from "react"
-import { useRecoilState } from "recoil"
+import { useRecoilValue } from "recoil"
 import Fab from "../general/Fab"
 import Modal from "../general/Modal"
 import AddPersonInput from "./AddPersonInput"
 import { personsState } from "./personModel"
-import { trash } from "ionicons/icons"
 
 const PersonsView: React.FC = () => {
-	const [persons, setPersons] = useRecoilState(personsState)
-	
-	const [isAddInputOpen, setIsAddInputOpen] = useState<boolean>(false)
+	const persons = useRecoilValue(personsState)
 
-	const deletePerson = (id: string) => {
-		setPersons((oldPersons) => {
-			const index = oldPersons.findIndex(p => p.id === id)
-			if (index === -1) {
-				throw new Error(`Could not find person to delete: id = ${id}`)
-			}
-			return [
-				...oldPersons.slice(0, index),
-				...oldPersons.slice(index + 1)
-					.map(p => ({ ...p, sortOrder: p.sortOrder - 1 })), // Adjust sort orders down
-			]
-		})
-	}
+	const [isAddInputOpen, setIsAddInputOpen] = useState<boolean>(false)
 
 	return (
 		<>
 			<IonList>
 				{persons.map(person => {
 					return (
-					<IonItemSliding key={person.id}>
-						<IonItem>{person.name}</IonItem>
-						<IonItemOptions side="start">
-							<IonItemOption color="danger" onClick={() => deletePerson(person.id)}>
-								<IonIcon slot="icon-only" icon={trash} size="large" />
-							</IonItemOption>
-						</IonItemOptions>
-					</IonItemSliding>
+						<IonItem key={person.id}>{person.name}</IonItem>
 					)
 				})}
 			</IonList>
--- src/tags/TagsView.tsx
+++ src/tags/TagsView.tsx
@@ -1,44 +1,22 @@
-import { IonIcon, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonList } from "@ionic/react"
+import { IonItem, IonList } from "@ionic/react"
 import { useState } from "react"
-import { useRecoilState } from "recoil"
+import { useRecoilValue } from "recoil"
 import Fab from "../general/Fab"
 import Modal from "../general/Modal"
 import AddTagInput from "./AddTagInput"
 import { tagsState } from "./tagModel"
-import { trash } from "ionicons/icons"
 
 const TagsView: React.FC = () => {
-	const [tags, setTags] = useRecoilState(tagsState)
+	const tags = useRecoilValue(tagsState)
 
 	const [isAddInputOpen, setIsAddInputOpen] = useState<boolean>(false)
-
-	const deleteTag = (id: string) => {
-		setTags((oldTags) => {
-			const index = oldTags.findIndex(t => t.id === id)
-			if (index === -1) {
-				throw new Error(`Could not find tag to delete: id = ${id}`)
-			}
-			return [
-				...oldTags.slice(0, index),
-				...oldTags.slice(index + 1)
-					.map(t => ({ ...t, sortOrder: t.sortOrder - 1 })), // Adjust sort orders down
-			]
-		})
-	}
 
 	return (
 		<>
 			<IonList>
 				{tags.map(tag => {
 					return (
-						<IonItemSliding key={tag.id}>
-							<IonItem>{tag.name}</IonItem>
-							<IonItemOptions side="start">
-								<IonItemOption color="danger" onClick={() => deleteTag(tag.id)}>
-									<IonIcon slot="icon-only" icon={trash} size="large" />
-								</IonItemOption>
-							</IonItemOptions>
-						</IonItemSliding>
+						<IonItem key={tag.id}>{tag.name}</IonItem>
 					)
 				})}
 			</IonList>
