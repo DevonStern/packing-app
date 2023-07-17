fd951e6bdd14d490af9336a1b98f0aba62ab2e05
devon.stern <devon.stern@gmail.com>
2023-07-17 08:35:23 -0600

    Resume normal syncing on interval and on app active

--- src/flags.ts
+++ src/flags.ts
@@ -2,5 +2,5 @@ export const logChangesToStoredData = true
 export const logChangesToServerData = true
 
 export const syncFlag = true
-export const syncInterval = 86400000
-export const syncOnAppActive = false //This is getting triggered about 10 times every time the app becomes active
+export const syncInterval = 60 * 1000
+export const syncOnAppActive = true
--- src/other/OtherPage.tsx
+++ src/other/OtherPage.tsx
@@ -1,23 +1,7 @@
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import OtherView from './OtherView';
-import useSync from '../sync/useSync';
-import { useSetRecoilState } from 'recoil';
-import { listsState } from '../lists/listModels';
 
 const OtherPage: React.FC = () => {
-	const { sync } = useSync()
-
-	const setValues = useSetRecoilState(listsState)
-	const initialSave = () => {
-		setValues(oldValues => oldValues.map(list => ({
-			...list,
-			items: list.items.map(item => ({
-				...item,
-				updatedOn: new Date(),
-			}))
-		})))
-	}
-
 	return (
 		<IonPage>
 			<IonHeader>
@@ -31,8 +15,6 @@ const OtherPage: React.FC = () => {
 						<IonTitle size="large">Other</IonTitle>
 					</IonToolbar>
 				</IonHeader>
-				<IonButton onClick={sync}>Sync</IonButton>
-				<IonButton onClick={initialSave}>ITEMS Initial Save and Upload</IonButton>
 				<OtherView />
 			</IonContent>
 		</IonPage>
